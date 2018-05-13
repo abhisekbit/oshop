a7e716121846333f97805dae3c72ec013e2ef9ab
Abhisek Das <abhisek@Abhiseks-MacBook-Pro.local>
2018-05-13 11:38:48 +0530

    Chapter20

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,25 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  user$: Observable<firebase.User>;
+  constructor(private afAuth: AngularFireAuth) {
 
-  ngOnInit() {
+    this.user$ = afAuth.authState;
   }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
 
 }
